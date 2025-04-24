fec68eadb269987a9220a3f50ca53541b7f92216
Aleksandr-avd <sanja1112221@mail.ru>
2025-04-24 16:11:40 +0300

    DZ3.0

--- lesson_03/lesson_3_task_3.py
+++ lesson_03/lesson_3_task_3.py
@@ -1,13 +1,10 @@
 from address import Address
 from mailing import Mailing
 
-if __name__ == "__main__":
-  
-    to_addr = Address("62701", "Воронеж", "Ватутина", "42", "5")
-    from_addr = Address("62960", "Москва", "Ленина", "456", "123")
+
+to_addr = Address("62701", "Воронеж", "Ватутина", "42", "5")
+from_addr = Address("62960", "Москва", "Ленина", "456", "123")
     
-  
-    mailing = Mailing(to_address=to_addr, from_address=from_addr, cost=5.99, track="TRACK0046")
+mailing = Mailing(to_address=to_addr, from_address=from_addr, cost=5.99, track="TRACK0046")
     
-    
-    print(mailing)
+print(mailing)
