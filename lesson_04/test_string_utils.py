271e67304e1ecb03fed8c2f08100d933711986ea
Aleksandr-Avd <sanja1112221@gmail.com>
2025-05-02 11:58:54 +0300

    dz v 2

--- lesson_04/test_string_utils.py
+++ lesson_04/test_string_utils.py
@@ -22,3 +22,62 @@ def test_capitalize_positive(input_str, expected):
 ])
 def test_capitalize_negative(input_str, expected):
     assert string_utils.capitalize(input_str) == expected
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, expected", [
+    (" Hello World", "Hello World"),
+    (" 123", "123"),
+    ("  Skypro", "Skypro"),
+])
+def test_trim_positive(input_str, expected):
+    assert string_utils.trim(input_str) == expected
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, expected",[ 
+    ("  Hello World! ", "Hello World! "),
+    ("123  ", "123  "),
+    ("  ", "")
+])
+def test_trim_negative(input_str, expected):
+    assert string_utils.trim(input_str) == expected
+
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected_result", [ 
+    ("Hello World","W", True),
+    ("123", "1", True),
+    ("Skypro", "o" ,True)
+
+])
+
+def test_contains_positive(input_str, symbol, expected_result):
+    assert string_utils.contains(input_str, symbol) == expected_result
+
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, symbol, expected_result", [
+    ("123", "a", False),
+    ("Python", "i", False),
+    ("", "o" ,False)
+
+])
+def test_contains_negative(input_str, symbol, expected_result):
+    assert string_utils.contains(input_str, symbol) == expected_result
+
+@pytest.mark.positive
+@pytest.mark.parametrize("input_str, symbol, expected_result", [ 
+    ("Hello World","World", "Hello "),
+    ("123", "1", "23"),
+    ("Skypro", "ro" ,"Skyp")
+])
+def test_delete_symbol_positive(input_str, symbol, expected_result):
+    assert string_utils.delete_symbol(input_str, symbol) == expected_result
+
+@pytest.mark.negative
+@pytest.mark.parametrize("input_str, symbol, expected_result", [
+    ("123", "i", "123" ),
+    ("Python", "i", "Python" ),
+    ("", "o" ,"")
+])
+def test_delete_symbol_negative(input_str, symbol, expected_result):
+    assert string_utils.delete_symbol(input_str, symbol) == expected_result
