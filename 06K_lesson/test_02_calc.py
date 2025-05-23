62fa6102da09fcb3661586987d12d96a7b3fe6b4
Aleksandr-Avd <sanja1112221@gmail.com>
2025-05-23 16:20:56 +0300

    dz6k2

--- 06K_lesson/test_02_calc.py
+++ 06K_lesson/test_02_calc.py
@@ -1,3 +1,4 @@
+import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
@@ -6,26 +7,23 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 
-driver = webdriver.Chrome(service =ChromeService(ChromeDriverManager().install()))
-
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-
-delay_input = driver.find_element(By.CSS_SELECTOR, "#delay")
-delay_input.clear()
-delay_input.send_keys("45")
-
-
-driver.find_element(By.XPATH, "//span[text()='7']").click()
-driver.find_element(By.XPATH, "//span[text()='+']").click()
-driver.find_element(By.XPATH, "//span[text()='8']").click()
-driver.find_element(By.CSS_SELECTOR, ".btn-outline-warning").click()
-
-wait = WebDriverWait(driver, 46)
-result = wait.until(
+def test_calculator():
+    driver = webdriver.Chrome(service =ChromeService(ChromeDriverManager().install()))
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    
+    delay_input = driver.find_element(By.CSS_SELECTOR, "#delay")
+    delay_input.clear()
+    delay_input.send_keys("45")
+    
+    driver.find_element(By.XPATH, "//span[text()='7']").click()
+    driver.find_element(By.XPATH, "//span[text()='+']").click()
+    driver.find_element(By.XPATH, "//span[text()='8']").click()
+    driver.find_element(By.CSS_SELECTOR, ".btn-outline-warning").click()
+    wait = WebDriverWait(driver, 46)
+    result = wait.until(
         EC.text_to_be_present_in_element((By.CSS_SELECTOR, ".screen"), "15")
-    )
-
-assert result, "Результат не равен 15"
-
-
-driver.quit()
+        )
+    assert result, "Результат не равен 15"
+    print(result)
+    
+    driver.quit()
